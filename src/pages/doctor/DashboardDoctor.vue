5ce352ad27cf2545adc1f812a2ab54be014b7dfc
David Fernando <mahefa.fernando@gmail.com>
2022-09-02 01:17:29 +0300

    Feat: Add Dashboard

---
+++ src/pages/doctor/DashboardDoctor.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-page class="flex column flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/Y-2.svg"
+      class="q-mb-lg"
+      style="width: 250px; height: 250px"
+    />
+    <q-spinner-facebook
+        color="primary"
+        size="7em"
+      />
+  </q-page>
+</template>
+
+<script>
+import { useQuasar } from "quasar";
+import { useRouter } from "vue-router";
+
+export default {
+  name: "IndexPage",
+  data() {
+    return {};
+  },
+  setup() {
+    const router = useRouter();
+    const $q = useQuasar();
+    let timer = null;
+
+    return {
+      router,
+      timer,
+      $q,
+    };
+  },
+  mounted() {
+    this.timer = setTimeout(() => {
+      this.timer = void 0;
+    });
+  },
+};
+</script>
